12fe671b3f51d021450e7b5cc78d697c2160fbce
bakxriyev <kamronbekbahriyev18@gmail.com>
2026-05-23 21:07:12 +0500

    fix: add GroupStudentModel to AttendanceModule imports

--- src/modules/attendence/attendence.module.ts
+++ src/modules/attendence/attendence.module.ts
@@ -6,9 +6,10 @@ import { StudentModel } from '../students/model/student.entity';
 import { GroupLessonModel } from '../group-lesson/entities/group-lesson.entity';
 import { AttendanceModel } from './model/attendence.entity';
 import { GroupModel } from '../groups/model/group.entity';
+import { GroupStudentModel } from '../group_student_model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([AttendanceModel, StudentModel, GroupLessonModel,GroupModel])],
+  imports: [SequelizeModule.forFeature([AttendanceModel, StudentModel, GroupLessonModel, GroupModel, GroupStudentModel])],
   providers: [AttendanceService],
   controllers: [AttendanceController],
 })
